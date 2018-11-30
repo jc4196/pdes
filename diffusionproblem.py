e80b3aa4710abee2e7fa4925308514b9b28dfbe7
James Cass <jc4196@bristol.ac.uk>
2018-11-30 14:17:48 +0000

    Stopping in the middle of trying to understand forcing functions which are functions of both x and t

--- diffusionproblem.py
+++ diffusionproblem.py
@@ -9,35 +9,7 @@ import numpy as np
 import matplotlib.pylab as pl
 from math import pi
 
-def tridiag(a, b, c, k1=-1, k2=0, k3=1):
-    # create a tridiagonal matrix on diagonals k1, k2, k3
-    return np.diag(a, k1) + np.diag(b, k2) + np.diag(c, k3)
-
-def BE(T, mx, mt, lmbda, u_0, lbc, rbc, f):
-    # Construct the backward Euler matrix
-    A_BE = tridiag((mx)*[-lmbda], (mx+1)*[1+2*lmbda], (mx)*[-lmbda])
-    
-    # Add the boundary conditions
-    A_BE[0,0] = A_BE[mx,mx] = 1
-    A_BE[0,1] = A_BE[mx,mx-1] = 0
-
-    deltat = T / (mt+1)
-    
-    u_jp1 = np.zeros(u_0.size)      # u at next time step 
-    u_j = u_0    # u at the current time step
-    
-    for n in range(1, mt+1):    
-        # Backward Euler timestep at inner mesh points
-        u_jp1 = np.linalg.solve(A_BE, u_j)
-
-        # Boundary conditions
-        u_jp1[0] = lbc(n*deltat); u_jp1[mx] = rbc(n*deltat) 
-            
-        # Update u_j
-        u_j[:] = u_jp1[:]
-        #u_j[1:mt] += deltat*f(n*deltat)
-   
-    return u_j
+from finitedifference import backwardeuler, forwardeuler, cranknicholson
 
 class DiffusionProblem:
     def __init__(self,
@@ -46,17 +18,17 @@ class DiffusionProblem:
                  ic=lambda x: np.sin(pi*x),
                  lbc=lambda t: 0,
                  rbc=lambda t: 0,
-                 f=lambda x: 0,
-                 solver=BE):
+                 f=lambda x, t: 0,
+                 fd=backwardeuler):
         self.kappa = kappa   # Diffusion constant
         self.L = L           # Length of interval
         self.ic = ic         # Initial condition u(x,0)
         self.lbc = lbc       # Left boundary condition u(0,t)
         self.rbc = rbc       # Right boundary condition u(L,t)
         self.f = f           # Forcing function f
-        self.solver = solver # Solving function (forward/backward euler..)
+        self.fd = fd         # Finite Difference method (forward/backward euler..)
         
-    def solve_to(self, T, mx=20, mt=1000):
+    def solve_to(self, T, mx, mt):
         x = np.linspace(0, self.L, mx+1)     # mesh points in space
         t = np.linspace(0, T, mt+1)     # mesh points in time
         deltax = x[1] - x[0]            # gridspacing in x
@@ -68,16 +40,34 @@ class DiffusionProblem:
         print("lambda =",lmbda)
     
         if isinstance(self.ic, (int, float)):
-            u_0 = np.full(x.size, self.ic)
+            u0 = np.full(x.size, self.ic, dtype=np.float64)
         else:
-            u_0 = self.ic(x)
+            u0 = self.ic(x)
+  
+        uT = self.fd(T, mx, mt, lmbda, u0,
+                     self.lbc, self.rbc, self.f)
+        return x, uT
 
-        print(u_0)    
-        u_T = self.solver(T, mx, mt, lmbda, u_0,
-                          self.lbc, self.rbc, self.f)
-        pl.plot(x, u_T)
+    def plot_at_T(self, T, mx=20, mt=1000, u_exact=None):
+        x, uT = self.solve_to(T,mx,mt)
+        pl.plot(x,uT,'ro',label='num')
+        
+        if u_exact:
+            x = np.linspace(0, self.L, 250)
+            pl.plot(x,u_exact(x,T),'b-',label='exact')
+        pl.xlabel('x')
+        pl.ylabel('u(x,{})'.format(T))
+        pl.title('Solution at time T = {}'.format(T))
+        pl.legend(loc='upper right')
         pl.show()
-        return x, u_T
 
-dp = DiffusionProblem(ic=0)
-dp.solve_to(0.3)
+kappa = 1
+L = 1
+        
+#u_exact = lambda x, t: np.exp(-kappa*(pi**2/L**2)*t)*np.sin(pi*x/L) + \
+#        0.5*np.exp(-kappa*9*(pi**2/L**2)*t)*np.sin(3*pi*x/L) 
+#dp = DiffusionProblem(1, 1, ic=lambda x: np.sin(pi*x) + 0.5*np.sin(3*pi*x))
+#dp.plot_at_T(0.2, u_exact=u_exact)
+
+dp = DiffusionProblem(rbc=lambda x:1, f = lambda x: 1, ic=0)
+dp.plot_at_T(0.35)
---
+++ finitedifference.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 13:56:51 2018
+
+@author: james
+"""
+
+import numpy as np
+
+def tridiag(a, b, c, k1=-1, k2=0, k3=1):
+    # create a tridiagonal matrix on diagonals k1, k2, k3
+    return np.diag(a, k1) + np.diag(b, k2) + np.diag(c, k3)
+
+def backwardeuler(T, mx, mt, lmbda, u_0, lbc, rbc, f):
+    # Construct the backward Euler matrix
+    A_BE = tridiag((mx)*[-lmbda], (mx+1)*[1+2*lmbda], (mx)*[-lmbda])
+    
+    # Add the boundary conditions
+    A_BE[0,0] = A_BE[mx,mx] = 1
+    A_BE[0,1] = A_BE[mx,mx-1] = 0
+    
+    u_jp1 = np.zeros(u_0.size)      # u at next time step 
+    u_j = u_0    # u at the current time step
+    
+    deltat = T / (mt+1)
+    # Solve the PDE: loop over all time points
+    for n in range(1, mt+1):    
+        # Backward Euler timestep at inner mesh points
+        u_jp1 = np.linalg.solve(A_BE, u_j)
+
+        # Boundary conditions
+        u_jp1[0] = lbc(n*deltat); u_jp1[mx] = rbc(n*deltat) 
+            
+        # Update u_j
+        u_j[:] = u_jp1[:]
+        u_j[1:mt] += deltat*f(n*deltat)
+   
+    return u_j
+
+def forwardeuler(T, mx, mt, lmbda, u_0, lbc, rbc, f):   
+    # Construct the forward Euler matrix
+    A_FE = tridiag((mx)*[lmbda], (mx+1)*[1-2*lmbda], (mx)*[lmbda])
+    A_FE[0,0] = A_FE[mx,mx] = 1
+    A_FE[0,1] = A_FE[mx,mx-1] = 0
+    
+    u_jp1 = np.zeros(u_0.size)      # u at next time step 
+    u_j = u_0    # u at the current time step
+    
+    deltat = T / (mt+1)
+    # Solve the PDE: loop over all time points
+    for n in range(1, mt+1):
+        # Forward Euler timestep at inner mesh points
+        u_jp1 = np.dot(A_FE, u_j)
+        
+        # Boundary conditions
+        u_jp1[0] = lbc(n*deltat); u_jp1[mx] = rbc(n*deltat)
+              
+        # Update u_j
+        u_j[:] = u_jp1[:]
+    
+    return u_j
+
+def cranknicholson(T, mx, mt, lmbda, u_0, lbc, rbc, f):
+    # Construct the Crank-Nicholson matrices
+    A_CN = tridiag((mx-2)*[-0.5*lmbda], (mx-1)*[1+lmbda], (mx-2)*[-0.5*lmbda])
+    B_CN = tridiag((mx-2)*[0.5*lmbda], (mx-1)*[1-lmbda], (mx-2)*[0.5*lmbda])
+    
+    u_jp1 = np.zeros(u_0.size)      # u at next time step 
+    u_j = u_0    # u at the current time step
+        
+    deltat = T / (mt+1)
+    for n in range(1, mt+1):    
+        # Crank-Nicholson timestep at inner mesh points
+        u_jp1[1:-1] = np.linalg.solve(A_CN, B_CN.dot(u_j[1:-1]))
+
+        # Boundary conditions
+        u_jp1[0] = lbc(n*deltat); u_jp1[mx] = rbc(n*deltat)
+            
+        # Update u_j
+        u_j[:] = u_jp1[:]    
+        u_j[1:mt] += 0.5*f(n*deltat)
+        
+    return u_j  
